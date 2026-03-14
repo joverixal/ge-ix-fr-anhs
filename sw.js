600156cbd89c4acffe9ba77b8bb942f491cce96b
joverixal <joverixal.entuna@gmail.com>
2026-03-14 14:19:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,63 +1,56 @@
-const CACHE_NAME = "gix-ix-bbs-app";
+const CACHE_NAME = "ge-ix-pos-v1";
 
 const FILES_TO_CACHE = [
-
-"./",
-"./index.html",
-"./pos.html",
-
-"./assets/js/config.js",
-"./assets/js/auth.js",
-"./assets/js/pos.js",
-
-"./assets/vendor/jquery.min.js",
-"./assets/vendor/bootstrap.bundle.min.js",
-"./assets/vendor/bootstrap.min.css",
-"./assets/vendor/toastr.min.js",
-"./assets/vendor/toastr.min.css",
-"./assets/vendor/bootstrap-icons.css",
-
-"./assets/vendor/fonts/bootstrap-icons.woff",
-"./assets/vendor/fonts/bootstrap-icons.woff2",
-
-"./assets/images/logo.png"
-
+  "./",
+  "./index.html",
+  "./pos.html",
+  "./assets/js/config.js",
+  "./assets/js/auth.js",
+  "./assets/js/pos.js",
+  "./assets/js/api.js",
+  "./assets/vendor/jquery.min.js",
+  "./assets/vendor/bootstrap.bundle.min.js",
+  "./assets/vendor/bootstrap.min.css",
+  "./assets/vendor/bootstrap-icons.css",
+  "./assets/vendor/toastr.min.js",
+  "./assets/vendor/toastr.min.css",
+  "./assets/vendor/fonts/bootstrap-icons.woff",
+  "./assets/vendor/fonts/bootstrap-icons.woff2",
+  "./assets/images/logo.png"
 ];
 
 self.addEventListener("install", event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME).then(cache => {
-
-return Promise.all(
-
-FILES_TO_CACHE.map(file => {
-
-return cache.add(file).catch(err => {
-console.warn("Failed to cache:", file);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => Promise.all(
+        FILES_TO_CACHE.map(file =>
+          cache.add(file).catch(err => console.warn("Failed to cache:", file))
+        )
+      ))
+  );
+  self.skipWaiting();
 });
 
-})
-
-);
-
-})
-
-);
-
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
-event.respondWith(
-
-caches.match(event.request).then(response => {
-
-return response || fetch(event.request);
-
-})
-
-);
-
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => {
+        // fallback to index.html for navigation requests
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      })
+  );
 });
