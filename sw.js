17be7688e0c327f6863deff447edcfd786eac35b
joverixal <joverixal.entuna@gmail.com>
2026-03-14 13:51:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,63 +1,63 @@
-const CACHE_NAME = "rixal-pos-v1";
+const CACHE_NAME = "gix-ix-bbs-app";
 
-const APP_SHELL = [
+const FILES_TO_CACHE = [
 
-"/",
-"/index.html",
-"/pos.html",
+"./",
+"./index.html",
+"./pos.html",
 
-"/assets/js/config.js",
-"/assets/js/auth.js",
-"/assets/js/pos.js",
+"./assets/js/config.js",
+"./assets/js/auth.js",
+"./assets/js/pos.js",
 
-"/assets/vendor/jquery.min.js",
-"/assets/vendor/bootstrap.bundle.min.js",
-"/assets/vendor/bootstrap.min.css",
-"/assets/vendor/bootstrap-icons.css",
-"/assets/vendor/toastr.min.js",
-"/assets/vendor/toastr.min.css",
+"./assets/vendor/jquery.min.js",
+"./assets/vendor/bootstrap.bundle.min.js",
+"./assets/vendor/bootstrap.min.css",
+"./assets/vendor/toastr.min.js",
+"./assets/vendor/toastr.min.css",
+"./assets/vendor/bootstrap-icons.css",
 
-"/assets/images/logo.png"
+"./assets/vendor/fonts/bootstrap-icons.woff",
+"./assets/vendor/fonts/bootstrap-icons.woff2",
+
+"./assets/images/logo.png"
 
 ];
 
 self.addEventListener("install", event => {
 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(APP_SHELL);
-    })
-  );
+event.waitUntil(
+
+caches.open(CACHE_NAME).then(cache => {
+
+return Promise.all(
+
+FILES_TO_CACHE.map(file => {
+
+return cache.add(file).catch(err => {
+console.warn("Failed to cache:", file);
+});
+
+})
+
+);
+
+})
+
+);
 
 });
 
 self.addEventListener("fetch", event => {
 
-  event.respondWith(
+event.respondWith(
 
-    caches.match(event.request).then(response => {
+caches.match(event.request).then(response => {
 
-      if (response) return response;
+return response || fetch(event.request);
 
-      return fetch(event.request).then(networkResponse => {
+})
 
-        return caches.open(CACHE_NAME).then(cache => {
-
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-
-        });
-
-      }).catch(() => {
-
-        if (event.request.mode === "navigate") {
-          return caches.match("/index.html");
-        }
-
-      });
-
-    })
-
-  );
+);
 
 });
