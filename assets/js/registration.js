c40c511f571030e89ce900de86672b4ee5780da6
joverixal <joverixal.entuna@gmail.com>
2026-03-15 13:03:40 +0800

    Create registration.js

---
+++ assets/js/registration.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+  toastr.options = {
+      "positionClass": "toast-bottom-center", // Bottom left corner
+      "timeOut": "3000",             // Auto hide after 3s
+      "extendedTimeOut": "1000",
+      "preventDuplicates": true
+  };
+
+  // Example: handle registration button
+  $('#btn-register').click(function(){
+      const runner = {
+          firstName: $('#inp-firstname').val(),
+          lastName: $('#inp-lastname').val(),
+          gender: $('#inp-gender').val(),
+          birthdate: $('#inp-birthdate').val(),
+          batchYear: $('#inp-batchyear').val(),
+          contact: $('#inp-contact').val(),
+          address: $('#inp-address').val(),
+          tshirtSize: $('#inp-tshirt').val()
+      };
+      
+      toastr.success("The registration has been successfully saved. For payment and activation, please contact your batch officer.");
+
+    // Wait 4 seconds (4000ms) before executing
+    setTimeout(function() {
+        window.location.href = "qrcode.html";
+    }, 5000);
+      
+  });
+
+});
